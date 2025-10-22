9a9aed04d1714030a70c97cddf599b2e023ba5b0
Mathieu Acher <mathieu.acher@irisa.fr>
2025-10-22 11:35:36 +0200

    turn: add Lua (+1 program)
    List-Digest: 100fcd11
    Model: meta-llama/llama-3.1-70b-instruct
    Temperature: 0.4

---
+++ languages/Lua/programs/6b8855d106de765d620776d2101b35c77eb858224f171c64868686c971f617bf/codemain.lua
@@ -0,0 +1,7 @@
+function love.load() end
+function love.update(dt) end
+function love.draw() end
+function love.keypressed(key) end
+function love.keyreleased(key) end
+function love.mousepressed(x, y, button) end
+function love.mousereleased(x, y, button) end

fae28ba33bca70f3e26e9d418ac2f8f29de1c5c7
Mathieu Acher <mathieu.acher@irisa.fr>
2025-10-22 11:41:25 +0200

    turn: add Objective-C (+1 program)
    List-Digest: 0095c316
    Model: openai/gpt-4o-mini
    Temperature: 0.4

---
+++ languages/Objective-C/programs/aa8feb1cf452dff3f8b7309309bea9a748eb5bfa4827f5f6efaaba8cc2dca554/code.m
@@ -0,0 +1,9 @@
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        NSString *greeting = @"Hello, World!";
+        NSLog(@"%@", greeting);
+    }
+    return 0;
+}
